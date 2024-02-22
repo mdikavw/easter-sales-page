4e48a05f110d3ac1d844f200951e71383922bf03
mdikavw <mdikavw@gmail.com>
2024-02-22 15:46:50 +0700

    [FEAT] Add mockup coloring book

--- includes/coloring-book-section.php
+++ includes/coloring-book-section.php
@@ -1,23 +1,19 @@
 <div class="container-fluid p-5 d-flex flex-column align-items-center bg-primary">
-    <h3 class="mb-5 fw-semibold">Coloring Book Bundle</h3>
-    <div class="row justify-content-center align-items-center mb-5 w-75">
-        <div class="col-md-6">
+    <h2 class="mb-5 fw-semibold">Coloring Book Bundle</h2>
+    <div class="row align-items-center mb-5 w-75">
+        <div class="col">
             <p>
                 Easter isn't just about egg hunts and chocolate bunnies—it's also a time for creativity and relaxation.
                 Coloring isn't just for kids; it's a proven stress-reliever for adults too, and a perfect addition to
                 your yoga routine.
-
-                As a business owner, you can draw in more customers and enhance your revenue by offering these free
-                Easter coloring pages. And why not take it a step further? Create beautiful Easter coloring books for
-                sale.
 
                 With this bundle, you'll receive 10 delightful Easter-themed coloring pages in both .png and .PSD
                 formats (2500 x 3300 pixels). Use them as is or tweak them to your heart's content. Let the Easter
                 spirit and creativity flow!"
             </p>
         </div>
-        <div class="col-md-6 d-flex justify-content-center">
-            <img src="https://via.placeholder.com/300x400" alt="Placeholder Image" />
+        <div class="col">
+            <img class="mw-100" src="asset/coloring-book/MockupColoringBook2.png" alt="Placeholder Image" />
         </div>
     </div>
     <div class="row justify-content-center">
--- includes/svg-section.php
+++ includes/svg-section.php
@@ -1,5 +1,5 @@
 <div class="container-fluid p-5 d-flex flex-column align-items-center">
-    <h3 class="mb-5 fw-semibold">25 Printable SVGs for your products</h3>
+    <h2 class="mb-5 fw-semibold">25 Printable SVGs for your products</h2>
 
     <div class="row justify-content-center align-items-center mb-5 px-5">
         <div class="col-md-6 d-flex justify-content-center">
@@ -10,10 +10,6 @@
                 We've simplified the process so you can effortlessly design t-shirts, mugs, notebooks, stickers, towels,
                 phone cases, and more. You have the flexibility to use these ready-made templates or, for advanced
                 customization, download the PSD files and make changes to your heart's content.
-
-                Never dipped your toes into product sales like this? Not a problem. We'll guide you through the process,
-                ensuring you can set up shop quickly without any initial investment or the need to keep physical product
-                inventory on hand.
 
                 Get ready to unleash your creativity and design an array of Easter-themed SVGs. These designs will make
                 your products stand out and elevate the holiday spirit in all your merchandise
